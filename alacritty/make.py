3a61389c7c84d3e41cd5431074bd06cc2b41fb7c
kiwixz <kiwixz@outlook.com>
2020-03-27 09:36:33 +0100

    [alacritty] use utils docker matrix build

--- alacritty/make.py
+++ alacritty/make.py
@@ -1,62 +1,32 @@
 #!/usr/bin/env python3
 
 import os
-import subprocess
+import sys
 
-
-IMAGE_TAG_BASE = "rope_alacritty"
-REF = "v0.4.1"
-VERSION = "0.4.1"
-
-
-DISTS = {
-    "bullseye": {
-        "base_image": "debian:bullseye-slim",
-        "apt_libs": "libexpat1-dev libfontconfig1-dev libfreetype-dev libxcb-xfixes0-dev",
-    },
-    "bionic": {
-        "base_image": "ubuntu:bionic",
-        "apt_libs": "libexpat1-dev libfontconfig1-dev libfreetype6-dev libxcb-xfixes0-dev",
-    },
-}
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import utils
 
 
 def main():
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-    for dist, dist_env in DISTS.items():
-        dockerfile = f"build/{dist}.Dockerfile"
-        os.makedirs("build", exist_ok=True)
-        with open("Dockerfile") as src:
-            with open(dockerfile, "w") as dst:
-                data = src.read()
-                for key, value in dist_env.items():
-                    data = data.replace(f"{{{{{key}}}}}", value)
-                dst.write(data)
+    version = "0.4.1"
 
-        image_tag = f"{IMAGE_TAG_BASE}_{dist}"
-        subprocess.check_call(
-            [
-                "docker",
-                "build",
-                "-t",
-                image_tag,
-                "-f",
-                dockerfile,
-                "--build-arg",
-                f"REF={REF}",
-                "--build-arg",
-                f"VERSION={VERSION}",
-                ".",
-            ]
-        )
-        container_id = subprocess.check_output(["docker", "create", image_tag]).decode().rstrip()
-        try:
-            out_dir = f"build/{dist}"
-            os.makedirs(out_dir, exist_ok=True)
-            subprocess.check_call(["docker", "cp", f"{container_id}:/root/alacritty-{VERSION}.deb", out_dir])
-        finally:
-            subprocess.check_call(["docker", "rm", container_id])
+    utils.matrix_docker_build(
+        "rope_alacritty",
+        {
+            "bullseye": {
+                "base_image": "debian:bullseye-slim",
+                "apt_libs": "libexpat1-dev libfontconfig1-dev libfreetype-dev libxcb-xfixes0-dev",
+            },
+            "bionic": {
+                "base_image": "ubuntu:bionic",
+                "apt_libs": "libexpat1-dev libfontconfig1-dev libfreetype6-dev libxcb-xfixes0-dev",
+            },
+        },
+        {"REF": f"v{version}", "VERSION": version},
+        [f"alacritty-{version}"],
+    )
 
 
 if __name__ == "__main__":
