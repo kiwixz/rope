ddd8a4ddbde31f56329c13167c18fa62f8134288
kiwixz <kiwixz@outlook.com>
2020-05-08 17:21:44 +0200

    [vscode-apt] add

---
+++ vscode-apt/make.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from typing import List
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import utils
+
+
+def main():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    version = "1.0"
+    utils.matrix_docker_build(
+        "vscode_alias",
+        {
+            "bullseye": {"base_image": "debian:bullseye-slim"},
+            "bionic": {"base_image": "ubuntu:bionic"},
+        },
+        {"VERSION": version},
+        [f"vscode-apt-{version}"],
+    )
+
+
+if __name__ == "__main__":
+    main()
